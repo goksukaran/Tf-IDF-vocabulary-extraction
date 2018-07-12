ec355ba85696b0bb3ac24553891f66228fb8eacc
Goksu KARA <goksukaran@gmail.com>
2018-07-12 20:52:51 +0200

    pdf parser plain text stage 1

--- PdfParser/PdfParser.py
+++ PdfParser/PdfParser.py
@@ -3,3 +3,10 @@ Created on 12 Jul 2018
 
 @author: goksukara
 '''
+from pdfrw import PdfReader
+import os 
+cwd = os.getcwd()
+
+x = PdfReader(cwd+'/Pdfs/BuildingautomationsystemsConceptsandtechnologyreview.pdf')
+
+print(x.pages[0].Annots[1])
