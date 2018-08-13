356ca01ad6749347399571659f6cbbf5babfe66d
Goksu KARA <goksukaran@gmail.com>
2018-08-13 18:02:54 +0200

    Big corpus colm read does not work

--- Preprocessing/OwnDevelopment/Integration/Preporcessing.py
+++ Preprocessing/OwnDevelopment/Integration/Preporcessing.py
@@ -5,12 +5,13 @@ Created on 6 Aug 2018
 '''
 import pandas as pd
 from PreprocessingClass import PreprocessingFunctions,Cleanupper 
+corpuspath='/Users/goksukara/Desktop/Projects/EclipseWorkspace/Specilization/PhytonCode/Data/'
 
 filename='corpus'
-for chunck_df in pd.read_csv(filename+'.csv',dtype={'filename':'str',"text":"str"}, chunksize=2,header=0,index_col=0, sep='\t'):
+for chunck_df in pd.read_csv(corpuspath+filename+'.csv',dtype={'file_name':'str',"text":"str"}, chunksize=10,header=0,index_col=0, sep='\t'):
     dataprocesing=PreprocessingFunctions(chunck_df)
-    print(chunck_df.columns.values)
+    print(dataprocesing.processed_data.loc[: , "text"])
     #dataprocesing.cleanup(Cleanupper())
-    print(dataprocesing.processed_data.columns.values)
+    #print(dataprocesing.processed_data.columns.values)
     dataprocesing.save(filename+'_preprocessed.csv')
     
